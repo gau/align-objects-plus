cf0edba24f235c9288d087210ace6b025445af24
gau <gau@MacBook-Pro.local>
2017-01-27 05:27:56 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,156 @@
+# README
+
+オブジェクトをできるだけ正確に整列します。
+
+<div class="fig center" style="margin-bottom: 20px;"><img src="http://www.graphicartsunit.com/saucer/images/align-objects-plus/eye.png" alt="イメージ" class="noshadow"></div>
+
+### 更新履歴
+
+* 0.5.0：新規作成（公開）
+
+----
+
+### 対応バージョン
+
+* Illustrator CS5／CS6／CC／CC 2014／CC 2015／CC 2015.3／CC 2017
+
+----
+
+### ダウンロード
+
+* [スクリプトをダウンロードする](https://github.com/gau/align-objects-plus/archive/master.zip)
+
+----
+
+### インストール方法
+
+1. ダウンロードしたファイルを解凍します。
+2. 所定の場所に「オブジェクトを整列Plus.jsx」をコピーします。Windows版ではお使いのIllustratorの種類によって保存する場所が異なりますのでご注意ください。
+3. Illustratorを再起動します。
+4. `ファイル` → `スクリプト` → `オブジェクトを整列Plus`と表示されていればインストール成功です。
+
+#### ファイルをコピーする場所
+
+| OS | バージョン | フォルダの場所 |
+|:-----|:-----|:-----|
+| Mac | 全 | /Applications/Adobe Illustrator *(ver)*/Presets/ja_JP/スクリプト/ |
+| 32bit Win | CS5まで | C:\Program Files\Adobe\Adobe Illustrator *(ver)*\Presets\ja_JP\スクリプト\ |
+| 64bit Win | CS5, CS6（32bit版） | C:\Program Files (x86)\Adobe\Adobe Illustrator *(ver)*\Presets\ja_JP\スクリプト\ |
+| 64bit Win | CS6（64bit版）以降 | C:\Program Files\Adobe\Adobe Illustrator *(ver)* (64 Bit)\Presets\ja_JP\スクリプト\ |
+
+* *(ver)*にはお使いのIllustratorのバージョンが入ります
+* 本スクリプトは、CS4以前では動作を検証しておりません
+
+----
+
+### 標準の整列機能ではテキストオブジェクトが正確に揃わない
+
+<div class="fig center"><img src="http://www.graphicartsunit.com/saucer/images/align-objects-plus/fig01.png" alt="標準の整列機能ではテキストオブジェクトが正確に揃わない" class="noshadow"></div>
+
+Illustrator標準の整列機能を使ってテキストオブジェクトを揃えようとすると、以下2つの問題にぶつかります。
+
+* 横書きテキストの場合、下に隙間ができてしまう（期待する位置より上に揃う）
+* トラッキングされた文字の場合、右端（下端）にトラッキング分の隙間ができてしまう（期待するより左、または上に揃う）
+
+いずれも、テキストのバウンディングボックス基準で整列位置が計算される仕様に基づくもので、Illustratorの整列がデタラメに処理されているわけではないのですが、おそらく現実として期待する結果になってないと感じることが多いのではないでしょうか。
+
+----
+
+### このスクリプトを使うと
+
+<div class="fig center"><img src="http://www.graphicartsunit.com/saucer/images/align-objects-plus/fig02.png" alt="このスクリプトを使うと" class="noshadow"></div>
+
+文字の（仮想）ボディのサイズを想定してテキストオブジェクトの大きさを再計算し、それに基づいて整列を行います。バウンディングボックスで文字の下に大きな隙間ができてしまうフォント（源ノ角ゴシックなど）や、トラッキングが設定されたテキストオブジェクトでも比較的正確な整列が可能です。
+
+なお、Illustratorとテキストのバウンディングボックスの関係について興味のある方は、[ものかの](http://tama-san.com/)さんさんのブログ記事を参考にしてみてください。
+
+* [IllustratorでSource Han Sansのバウンディングボックスが縦長になる理由](http://tama-san.com/illustrator-boundingbox/)
+
+----
+
+### 使い方
+
+<div class="fig center"><img src="http://www.graphicartsunit.com/saucer/images/align-objects-plus/fig03.png" alt="使い方" class="noshadow"></div>
+
+1. オブジェクトを選択してスクリプトを実行すると、整列のダイアログが開きます
+2. ［水平方向］、［垂直方向］で、整列したい位置のチェックボックスをクリックします。整列をしたくないときは、オンになっているチェックボックスを再度クリックして、チェックをオフにします
+3. ［基準となる範囲］のメニューから、整列の基準としたい対象を選択します。`アートボード`、`選択範囲`、`最背面オブジェクト`、`最前面オブジェクト`の4つから選択可能です
+4. 整列基準の範囲を確認したいときは`整列の範囲をハイライト`のチェックをオンにします。対象範囲がピンクにハイライトされます
+5. `実行`をクリックすると整列を実行します
+
+#### 基準となる範囲の詳細
+
+| 項目 | 説明 |
+|:-----|:-----|:-----|
+| アートボード | 現在アクティブなアートボードの大きさ |
+| 選択範囲 | 選択されたオブジェクト全体の範囲 |
+| 最背面オブジェクト | 選択された中で最も背面にあるオブジェクトの大きさ |
+| 最前面オブジェクト | 選択された中で最も前面にあるオブジェクトの大きさ |
+
+----
+
+### ダイアログについて
+
+スクリプトは最後に実行したダイアログの状態を記憶しますので、スクリプトを起動したあとにすぐreturn（enter）キーを押せば前回と同じ設定で整列を実行できます。さらに、`option（Alt）`キーを押しながらスクリプトを起動することで、ダイアログを省略しつつ前回と同じ整列を実行可能です。
+
+----
+
+### カスタマイズ
+
+<div class="fig center"><img src="http://www.graphicartsunit.com/saucer/images/align-objects-plus/fig04.png" alt="カスタマイズ" class="noshadow"></div>
+
+通常の実行でもダイアログ表示をカットするようにカスタマイズできます。この場合は、あらかじめ設定した値に基づいて整列が実行されます。スクリプトをテキストエディタで開き、10行目あたりにある`settings`の各値を変更してください。ダイアログ表示をカットする場合の整列位置や基準は、ここで設定した値が使われます。
+
+| キー | 型 | 説明 | 値 |
+|:-----|:-----|:-----|:-----|
+| vertical | Number | 水平方向の揃え | -1：移動なし｜0：左｜1：中央｜2：右 |
+| horizontal | Number | 垂直方向の揃え | -1：移動なし｜0：左｜1：中央｜2：右 |
+| base | Number | 整列の基準 | 0：アートボード｜1：選択範囲｜2：最前面オブジェクト｜3：最背面オブジェクト |
+| previewArea | Boolean | 整列基準範囲のハイライト表示するか | false：しない｜true：する |
+| showDialog | Boolean | ダイアログを表示するか | false：しない｜true：する |
+
+* ダイアログ表示をカットしたい場合は、`showDialog`を`false`にしてください
+* ダイアログ表示をカット時`previewArea`の値は使われません
+
+----
+
+### キーボードショートカットのすすめ
+
+ダイアログを省略して実行するときは特にですが、スクリプトをキーボードショートカットで実行できるようにしておくことをおすすめします。Illustrator標準ではスクリプトにショートカットキーを割り当てできないので、Macの方は、[ものかの](http://tama-san.com/)さんの「SPAi」や、Keyboard Maestroなどのツールを使うといいでしょう。Keyboard Maestroは、[三階ラボ](http://3fl.jp/)さんの記事が詳しくて参考になります。Windowsの方は……なんとか頑張ってください！（ツールがあるのかどうか知らないです。すみません）
+
+* [SPAi | Script Panel for Illstrator](http://tama-san.com/spai/)
+* [3flab inc. | Keyboard Maestro の使い方 – 基本機能の説明編](http://3fl.jp/nkm001)
+
+----
+
+### その他
+
+* クリッピングマスクされたオブジェクトは、クリッピングパスの大きさを基準とします
+* エリア内文字はテキストエリアを整列の基準とします
+
+----
+
+### 注意
+
+* CS6ではダイアログ表示直後のプレビューが乱れることがありますが、何かの設定を一度変更すれば正常に戻ります
+* 必要なオブジェクトが選択されていないときは、警告を表示して処理を中断します
+* オブジェクトの種類や構造によって意図しない結果になる可能性もゼロではありません
+* 大量のオブジェクトでは処理に時間がかかることがあります
+
+----
+
+### 免責事項
+
+* このスクリプトを使って起こったいかなる現象についても制作者は責任を負えません。すべて自己責任にてお使いください。
+* CS5、CS6、CC 2015.3、CC 2017（いずれもMac版）で動作の確認はしましたが、OSのバージョンやその他の状況によって実行できないことがあるかもしれません。もし動かなかったらごめんなさい。
+
+----
+
+### ライセンス
+
+* オブジェクトを整列Plus.jsx
+* Copyright (c) 2017 Toshiyuki Takahashi
+* Released under the MIT license
+* [http://opensource.org/licenses/mit-license.php](http://opensource.org/licenses/mit-license.php)
+* Created by Toshiyuki Takahashi ([Graphic Arts Unit](http://www.graphicartsunit.com/))
+* [Twitter](https://twitter.com/gautt)
---
+++ オブジェクトを整列Plus.jsx
@@ -0,0 +1,516 @@
+/*
+オブジェクトを整列Plus.jsx
+Copyright (c) 2017 Toshiyuki Takahashi
+Released under the MIT license
+http://opensource.org/licenses/mit-license.php
+http://www.graphicartsunit.com/
+*/
+(function() {
+
+	// Settings
+	var settings = {
+		'horizontal' : 1,
+		'vertical' : 1,
+		'base' : 3,
+		'previewArea' : false,
+		'showDialog' : true
+	};
+
+	// ダイアログ非表示で実行するときは、上記 settings の値で挙動を設定
+	// 'vertical'    -> 水平方向［-1:移動なし｜0:左｜1:中央｜2:右］
+	// 'horizontal'  -> 垂直方向［-1:移動なし｜0:上｜1:中段｜2:下］
+	// 'base'        -> 整列の基準［0:アートボード｜1:選択範囲｜2:最前面オブジェクト｜3:最背面オブジェクト］
+	// 'previewArea' -> 整列基準の範囲をハイライトする［false:しない｜true:する］
+	// 'showDialog'  -> ダイアログを表示する［false:しない｜true:する］
+
+	// Constant
+	const SCRIPT_TITLE = 'オブジェクトを整列Plus';
+	const SCRIPT_VERSION = '0.5.0';
+	const PEVIEW_LAYERNAME = '_gau_Align_Area_Preview_';
+	const ILLUSTRATOR_VERSION = Number(app.version.split('.')[0]);
+
+	// Document and selection
+	var doc = app.activeDocument;
+	var sel = doc.selection;
+
+	// UI Dialog
+	function MainDialog() {
+		this.init();
+		return this;
+	};
+	MainDialog.prototype.init = function() {
+
+		var unit = 20;
+		var thisObj = this;
+
+		thisObj.dlg = new Window('dialog', SCRIPT_TITLE + ' - ver.' + SCRIPT_VERSION);
+		thisObj.dlg.margins = [unit * 1.5, unit * 1.5, unit * 1.5, unit * 1.5];
+
+		thisObj.panelHorizontal = thisObj.dlg.add('panel', undefined, '水平方向：');
+		thisObj.panelHorizontal.margins = [unit, unit, unit, unit];
+		thisObj.panelHorizontal.alignment = 'left';
+		thisObj.panelHorizontal.orientation = 'row';
+		thisObj.panelHorizontal.name = 'horizontal-group';
+
+		thisObj.panelVertical = thisObj.dlg.add('panel', undefined, '垂直方向：');
+		thisObj.panelVertical.margins = [unit, unit, unit, unit];
+		thisObj.panelVertical.alignment = 'left';
+		thisObj.panelVertical.orientation = 'row';
+		thisObj.panelVertical.name = 'vertical-group';
+
+		thisObj.optionGroup = thisObj.dlg.add('group', undefined);
+		thisObj.optionGroup.margins = [0, 0, 0, 0];
+		thisObj.optionGroup.alignment = 'center';
+		thisObj.optionGroup.orientation = 'column';
+		thisObj.optionGroup.name = 'option-group';
+
+		thisObj.options = [
+			thisObj.optionGroup.add('group', undefined),
+			thisObj.optionGroup.add('group', undefined)
+		];
+
+		thisObj.buttonGroup = thisObj.dlg.add('group', undefined);
+		thisObj.buttonGroup.margins = [unit, unit, unit, unit * 0];
+		thisObj.buttonGroup.alignment = 'center';
+		thisObj.buttonGroup.orientation = 'row';
+		thisObj.buttonGroup.name = 'button-group';
+
+		// horizontal direction 
+		thisObj.lineHorizontal = [
+			thisObj.panelHorizontal.add('checkbox', undefined, '左'),
+			thisObj.panelHorizontal.add('checkbox', undefined, '中央'),
+			thisObj.panelHorizontal.add('checkbox', undefined, '右')
+		];
+		if(settings.horizontal > thisObj.lineHorizontal.length - 1 || isNaN(settings.horizontal)) {
+			settings.horizontal = -1;
+		} else {
+			settings.horizontal = Math.floor(settings.horizontal);
+		}
+		for (var key in thisObj.lineHorizontal) {
+			thisObj.lineHorizontal[key].name = 'horizontal-' + key;
+			thisObj.lineHorizontal[key].minimumSize = [70, undefined];
+			thisObj.lineHorizontal[key].value = false;
+			thisObj.lineHorizontal[key].alignment = 'left';
+			thisObj.lineHorizontal[key].addEventListener('click', preview);
+			thisObj.lineHorizontal[key].onClick = function(){
+					if(ILLUSTRATOR_VERSION >= 20) this.dispatchEvent(new UIEvent('click'));
+			};
+		}
+		if(thisObj.lineHorizontal[settings.horizontal]) thisObj.lineHorizontal[settings.horizontal].value = true;
+
+		// Vertical direction 
+		thisObj.lineVertical = [
+			thisObj.panelVertical.add('checkbox', undefined, '上'),
+			thisObj.panelVertical.add('checkbox', undefined, '中段'),
+			thisObj.panelVertical.add('checkbox', undefined, '下')
+		];
+		if(settings.vertical > thisObj.lineVertical.length - 1 || isNaN(settings.vertical)) {
+			settings.vertical = -1;
+		} else {
+			settings.vertical = Math.floor(settings.vertical);
+		}
+		for (var key in thisObj.lineVertical) {
+			thisObj.lineVertical[key].name = 'vertical-' + key;
+			thisObj.lineVertical[key].minimumSize = [70, undefined];
+			thisObj.lineVertical[key].value = false;
+			thisObj.lineVertical[key].alignment = 'left';
+			thisObj.lineVertical[key].addEventListener('click', preview);
+			thisObj.lineVertical[key].onClick = function(){
+					if(ILLUSTRATOR_VERSION >= 20) this.dispatchEvent(new UIEvent('click'));
+			};
+		}
+		if(thisObj.lineVertical[settings.horizontal]) thisObj.lineVertical[settings.horizontal].value = true;
+
+		// Base area 
+		thisObj.options[0].add('statictext', undefined, '整列の基準：');
+		thisObj.options[0].alignment = 'left';
+		thisObj.options[0].margins = [unit * 0, 0, unit * 0, unit];
+		thisObj.alignBase = thisObj.options[0].add('dropdownlist', undefined, ['アートボード', '選択範囲', '最前面オブジェクト',　'最背面オブジェクト']);
+		thisObj.alignBase.selection = settings.base;
+		thisObj.alignBase.addEventListener('change', preview);
+
+		thisObj.options[1].alignment = 'left';
+		thisObj.previewArea = thisObj.options[1].add('checkbox', undefined, '整列の範囲をハイライト');
+		thisObj.previewArea.name = 'previewArea';
+		thisObj.previewArea.value = settings.previewArea;
+		thisObj.previewArea.addEventListener('click', preview);
+		thisObj.previewArea.onClick = function(){
+				if(ILLUSTRATOR_VERSION >= 20) this.dispatchEvent(new UIEvent('click'));
+		};
+
+		thisObj.cancel = thisObj.buttonGroup.add('button', undefined, 'キャンセル');
+		thisObj.ok = thisObj.buttonGroup.add('button', undefined, '実行');
+
+		// Preview
+		function preview(event) {
+			if(event) {
+				switch(event.target.name) {
+					case 'horizontal-0' :
+					case 'horizontal-1' :
+					case 'horizontal-2' :
+						if(settings.horizontal !== -1 && thisObj.lineHorizontal[settings.horizontal].value) thisObj.lineHorizontal[settings.horizontal].value = false;
+						break;
+					case 'vertical-0' :
+					case 'vertical-1' :
+					case 'vertical-2' :
+						if(settings.vertical !== -1 && thisObj.lineVertical[settings.vertical].value) thisObj.lineVertical[settings.vertical].value = false;
+						break;
+					default :
+						break;
+				}
+			}
+			thisObj.updateSettings();
+			try {
+				mainProcess(true);
+			} catch(er) {
+				showError(er);
+			}
+			var activeLayer = doc.activeLayer;
+			var dummyLayer = doc.layers.add();
+			doc.activeLayer = activeLayer;
+			dummyLayer.remove();
+			app.redraw();
+			app.undo();
+		}
+		preview(null);
+
+		// Add event handler to buttons
+		thisObj.ok.onClick = function() {
+			try {
+				mainProcess(false);
+			} catch(er) {
+				showError(er);
+			}
+			thisObj.closeDialog();
+		}
+		thisObj.cancel.onClick = function() {
+			thisObj.closeDialog();
+		}
+
+	};
+	MainDialog.prototype.updateSettings = function() {
+		settings.horizontal = this.getSelectedIndex(this.lineHorizontal);
+		settings.vertical = this.getSelectedIndex(this.lineVertical);
+		settings.base = this.alignBase.selection.index;
+		settings.previewArea = this.previewArea.value;
+	};
+	MainDialog.prototype.getSelectedIndex = function(array) {
+		for (var i = 0; i < array.length; i++) {
+			if(array[i].value) return i;
+		}
+		return -1;
+	};
+	MainDialog.prototype.showDialog = function() {
+		this.dlg.show();
+	};
+	MainDialog.prototype.closeDialog = function() {
+		this.dlg.close();
+	};
+
+	// Main process
+	function mainProcess(preview) {
+
+		// Get target objects and properties
+		var targets = getTargetObjects(sel);
+		var targetsProps = [];
+		for (var i = 0; i < targets.length; i++) {
+			targetsProps.push(getProperties(targets[i]));
+		}
+
+		// Get base area properties
+		var baseProp = {};
+		switch (settings.base) {
+			case 0 :
+				// Artboard
+				baseProp = getProperties(doc.artboards[doc.artboards.getActiveArtboardIndex()]);
+				break;
+			case 1 :
+				// Selection
+				baseProp = {'bounds' : getMaxBounds(targets)};
+				baseProp.size = [baseProp.bounds[2] - baseProp.bounds[0], (baseProp.bounds[3] - baseProp.bounds[1]) * -1];
+				baseProp.center = [baseProp.bounds[2] - baseProp.size[0] / 2, (baseProp.bounds[3] + baseProp.size[1] / 2) * -1];
+				break;
+			case 2 :
+				// Front object
+				baseProp = getProperties(targets[0]);
+				break;
+			case 3 :
+				// Back object
+				baseProp = getProperties(targets[targets.length-1]);
+				break;
+			default :
+				break;
+		}
+
+		// Calculate the gap distance
+		for(var key in targetsProps) {
+			targetsProps[key].gap = [0, 0];
+			switch(settings.horizontal) {
+				case 0 :
+					targetsProps[key].gap[0] = baseProp.bounds[0] - targetsProps[key].bounds[0];
+					break;
+				case 1 :
+					targetsProps[key].gap[0] = baseProp.center[0] - targetsProps[key].center[0];
+					break;
+				case 2 :
+					targetsProps[key].gap[0] = baseProp.bounds[2] - targetsProps[key].bounds[2];
+					break;
+				default :
+					break;
+			}
+			switch(settings.vertical) {
+				case 0 :
+					targetsProps[key].gap[1] = baseProp.bounds[1] - targetsProps[key].bounds[1];
+					break;
+				case 1 :
+					targetsProps[key].gap[1] = (baseProp.center[1] - targetsProps[key].center[1]) * -1;
+					break;
+				case 2 :
+					targetsProps[key].gap[1] = baseProp.bounds[3] - targetsProps[key].bounds[3];
+					break;
+				default :
+					break;
+			}
+
+			// Translate the target
+			targetsProps[key].item.translate(targetsProps[key].gap[0], targetsProps[key].gap[1]);
+
+		}
+
+		if(preview) {
+			// Draw preview area
+			if(settings.previewArea) drawPreviewArea(baseProp.bounds);
+		} else {
+			// Save Settings
+			if(settings.showDialog) saveSettings();
+		}
+
+	}
+
+	// Get target objects
+	function getTargetObjects(o) {
+		var targets = [];
+		for(var key in o) {
+			targets.push(o[key]);
+		}
+		return targets;
+	}
+
+	// Get the bounds from target
+	function getTheBounds(item) {
+		var bounds = item.geometricBounds;
+		if(item.typename === 'TextFrame') {
+			bounds = getTextItemBounds(item);
+		} else if(item.typename === 'GroupItem') {
+			bounds = item.clipped ? getClippingPath(item.pageItems).geometricBounds : getMaxBounds(item.pageItems);
+		} else if(item.typename === 'Artboard') {
+			bounds = item.artboardRect;
+		}
+		return bounds;
+	}
+
+	// Get the properties from target
+	function getProperties(item) {
+		var bounds = getTheBounds(item);
+		var size = [bounds[2] - bounds[0], Math.abs(bounds[3] - bounds[1])];
+		var props = {
+			item : item,
+			bounds : bounds,
+			size : size,
+			center : [bounds[2] - size[0] / 2, (bounds[3] + size[1] / 2) * -1]
+		};
+		return props;
+	}
+
+	// Get the clipping path from ClipGroup
+	function getClippingPath(items) {
+		for (var i = 0; i < items.length; i++) {
+			if(items[i].clipping) return items[i];
+		}
+		return false;
+	}
+
+	// Get the max bounds from objects
+	function getMaxBounds(items) {
+		var boundsArray = [[], [], [], []];
+		for (var i = 0; i < items.length; i++) {
+			var bounds = getTheBounds(items[i]);
+			for (var j = 0; j < bounds.length; j++) {
+				boundsArray[j].push(bounds[j]);
+			}
+		}
+		return [Math.min.apply(null, boundsArray[0]), Math.max.apply(null, boundsArray[1]), Math.max.apply(null, boundsArray[2]), Math.min.apply(null, boundsArray[3])];
+	}
+
+	// Get the bounds from TextFrame
+	function getTextItemBounds(item) {
+		if(item.orientation == TextOrientation.VERTICAL) return item.geometricBounds;
+
+		var trackings = [];
+		var charaAttrs = [];
+		var dummyItem = item.duplicate();
+
+		for(var j = 0; j < dummyItem.lines.length; j++) {
+			charaAttrs[j] = dummyItem.lines[j].characters[dummyItem.lines[j].characters.length-1].characterAttributes;
+			trackings[j] = charaAttrs[j].tracking;
+			var dummyColor = new NoColor();
+			if(charaAttrs[j].fillColor.typename == 'NoColor') dummyColor = new RGBColor();
+			charaAttrs[j].fillColor = dummyColor;
+			charaAttrs[j].tracking = 0;
+		}
+
+		var charaAttr = getMaxCharacter(dummyItem.lines[dummyItem.lines.length-1].characters);
+		var heightGap = getTextHeight(charaAttr) - Math.max.apply(null, charaAttr.totalSize);
+		var bounds = [dummyItem.geometricBounds[0], dummyItem.geometricBounds[1], dummyItem.geometricBounds[2], dummyItem.geometricBounds[3] + heightGap];
+		dummyItem.remove();
+		return bounds;
+	}
+
+	// Get the height form character
+	function getTextHeight(charaAttr) {
+		var activeLayer = doc.activeLayer;
+		var dummyLayer = doc.layers.add();
+		var tf = dummyLayer.textFrames.add();
+		tf.name = "_temp_textframe_";
+		tf.contents = "D";
+		tf.textRange.characterAttributes.size = Math.max.apply(null, charaAttr.totalSize);
+		tf.textRange.characterAttributes.textFont = charaAttr.textFont[0];
+		var tempHeight = (-tf.geometricBounds[3] + tf.geometricBounds[1]);
+		tf.remove();
+		dummyLayer.remove();
+		doc.activeLayer = activeLayer;
+		return tempHeight;
+	}
+
+	// Get max size form character
+	function getMaxCharacter(chara) {
+		var ca = {'size':[], 'baselineShift':[], 'textFont':[], 'horizontaltalScale':[], 'verticalScale':[], 'totalSize':[]};
+		for (var i = 0; i < chara.length; i++) {
+			ca.size.push(chara[i].characterAttributes.size);
+			ca.baselineShift.push(chara[i].characterAttributes.baselineShift);
+			ca.textFont.push(chara[i].characterAttributes.textFont);
+			ca.horizontaltalScale.push(chara[i].characterAttributes.horizontaltalScale);
+			ca.verticalScale.push(chara[i].characterAttributes.verticalScale);
+			ca.totalSize.push(chara[i].characterAttributes.size * chara[i].characterAttributes.verticalScale / 100);
+		}
+		return ca;
+	}
+
+	// Draw preview area
+	function drawPreviewArea(bounds) {
+		var stroke = false;
+		var activeLayer = doc.activeLayer;
+		var alignAreaLayer = doc.layers.add();
+		alignAreaLayer.name = PEVIEW_LAYERNAME;
+		var rect = doc.pathItems.rectangle(bounds[1], bounds[0], bounds[2] - bounds[0], -bounds[3] + bounds[1]);
+		doc.activeLayer = activeLayer;
+		var color;
+		var dcs = doc.documentColorSpace;
+		if(dcs == DocumentColorSpace.CMYK) {
+			color = new CMYKColor();
+			color.black = 0;
+			color.cyan = 0;
+			color.magenta = 100;
+			color.yellow = 0;
+		} else {
+			color = new RGBColor();
+			color.red = 255;
+			color.green = 0;
+			color.blue = 128;
+		}
+		if(stroke) {
+			rect.stroked = true;
+			rect.filled = false;
+			rect.strokeColor = color;
+			rect.strokeDashes = [4,2];
+			rect.strokeWidth = 2;
+		} else {
+			rect.stroked = false;
+			rect.filled = true;
+			rect.fillColor = color;
+			rect.opacity = 30;
+		}
+		return rect;
+	}
+
+	function showError(er) {
+		alert('エラーが発生しましたので処理を中止します\nエラー内容：' + er);
+	}
+
+	// Load setting from json file
+	var saveOptions = {
+		'os' : File.fs,
+		'jsxPath' : $.fileName,
+		'reverseDomain' : 'com.graphicartsunit',
+		'fileName' : 'align_objects_plus.json',
+		'path' : ''
+	};
+
+	if(settings.showDialog) {
+		saveOptions.path = getSettingFilePath(saveOptions);
+		settings = loadSettings() ? loadSettings() : settings;
+	}
+
+	// Get path of json file
+	function getSettingFilePath(options) {
+		var filepath = '';
+		switch(options.os) {
+			case 'Macintosh':
+				filepath = Folder.userData + '/' + options.reverseDomain + '/Illustrator/Scripts/' + options.fileName;
+				break;
+			case 'Windows':
+				filepath = Folder.userData + '/' + options.reverseDomain + '/Illustrator/Scripts' + options.fileName;
+				break;
+			default :
+				break;
+		}
+		return filepath;
+	}
+
+	// Load settings from json file
+	function loadSettings() {
+		if(new File(saveOptions.path).exists) {
+			var settingFile = new File(saveOptions.path);
+			settingFile.encoding = 'UTF-8';
+			settingFile.open('r');
+			var loadedSettings = settingFile.readln();
+			loadedSettings = (new Function('return' + loadedSettings))();
+			settingFile.close();
+			return loadedSettings;
+		} else {
+			return false;
+		}
+	}
+
+	// Save settings to json file
+	function saveSettings() {
+		var dir = saveOptions.path.match(/(.*)(\/)/)[1];
+		if(!new Folder(dir).exists) {
+			new Folder(dir).create();
+		}
+		var settingFile = new File(saveOptions.path);
+		settingFile.open('w');
+		settingFile.write(settings.toSource());
+		settingFile.close();
+	}
+
+	// Show dialog
+	var dialog;
+	if (!doc) {
+		alert('対象ドキュメントがありません');
+	} else if (sel.length <= 0) {
+		alert('オブジェクトが選択されていません');
+	} else {
+		if(settings.showDialog) {
+			dialog = new MainDialog();
+			dialog.showDialog();
+		} else {
+			try {
+				mainProcess(false);
+			} catch(er) {
+				showError(er);
+			}
+		}
+	}
+
+}());
